37854bf289e39839dfc05dc4287813e9eaccd693
James Collerton <jamescollerton@live.co.uk>
2021-03-23 17:57:26 +0000

    Added project shell

---
+++ src/main/java/com/example/kinesiscamel/KinesiscamelApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinesiscamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesiscamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesiscamelApplication.class, args);
+	}
+
+}
