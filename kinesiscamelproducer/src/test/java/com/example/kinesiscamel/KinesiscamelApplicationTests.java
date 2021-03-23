bf9385bfce6c22d0f7422fb0f9fa08bbf2b13ba1
James Collerton <jamescollerton@live.co.uk>
2021-03-23 19:00:15 +0000

    Separated out two consumer and producer components

---
+++ kinesiscamelproducer/src/test/java/com/example/kinesiscamel/KinesiscamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinesiscamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesiscamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
