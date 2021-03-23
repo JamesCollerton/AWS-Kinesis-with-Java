1954be9fef53479bc7188c4de030463c4ca74f99
James Collerton <jamescollerton@live.co.uk>
2021-03-23 22:19:48 +0000

    Cleaner basic implementation

--- kinesiscamelproducer/src/main/java/com/example/kinesiscamel/initialization/TwitterInitialization.java
+++ kinesiscamelproducer/src/main/java/com/example/kinesiscamel/initialization/TwitterInitialization.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @Component
@@ -20,17 +18,26 @@ public class TwitterInitialization implements ApplicationListener<ContextRefresh
   @Value("${twitter-api.search-url}")
   private String twitterApiSearchUrl;
 
+  @Value("${twitter-api.recent-path}")
+  private String twitterApiRecentPath;
+
   @Autowired private RestTemplate restTemplate;
 
   @Override
   public void onApplicationEvent(ContextRefreshedEvent event) {
-    log.info("Searching for tweets");
+    log.info("Searching for tweets, composing URI");
+
+    String uriString =
+        UriComponentsBuilder.fromHttpUrl(
+                String.format("%s%s", twitterApiSearchUrl, twitterApiRecentPath))
+            .queryParam("query", "to:bbc")
+            .toUriString();
+
+    log.info("Composed URI {}", uriString);
+
     ResponseEntity<TwitterApiSearchResult> twitterApiSearchResultResponseEntity =
-        restTemplate.exchange(
-            String.format("%s%s", twitterApiSearchUrl, "/recent?query=to:bbc"),
-            HttpMethod.GET,
-            new HttpEntity<>(new HttpHeaders()),
-            TwitterApiSearchResult.class);
+        restTemplate.getForEntity(uriString, TwitterApiSearchResult.class);
+
     log.info("{}", twitterApiSearchResultResponseEntity.getBody());
   }
 }
