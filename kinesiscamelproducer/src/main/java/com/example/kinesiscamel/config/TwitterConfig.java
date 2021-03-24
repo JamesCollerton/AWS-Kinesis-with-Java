410acb33eeac8b1383334d08f656339c395813d1
James Collerton <jamescollerton@live.co.uk>
2021-03-24 09:32:32 +0000

    Got the initial endpoint working

--- kinesiscamelproducer/src/main/java/com/example/kinesiscamel/config/TwitterConfig.java
+++ kinesiscamelproducer/src/main/java/com/example/kinesiscamel/config/TwitterConfig.java
@@ -2,10 +2,7 @@ package com.example.kinesiscamel.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Configuration
@@ -13,21 +10,4 @@ public class TwitterConfig {
 
   @Value("${twitter-api.bearer-token}")
   private String bearerToken;
-
-  @Bean
-  public RestTemplate getRestTemplate(
-      @Value("${twitter-api.bearer-token}") final String bearerToken) {
-    log.info(bearerToken);
-    return new RestTemplateBuilder(
-            rt ->
-                rt.getInterceptors()
-                    .add(
-                        (request, body, execution) -> {
-                          request
-                              .getHeaders()
-                              .add("Authorization", String.format("Bearer %s", bearerToken));
-                          return execution.execute(request, body);
-                        }))
-        .build();
-  }
 }
---
+++ kinesiscamelproducer/src/main/java/com/example/kinesiscamel/routes/TwitterRoute.java
@@ -0,0 +1,29 @@
+package com.example.kinesiscamel.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TwitterRoute extends RouteBuilder {
+
+  @Value("${twitter-api.bearer-token}")
+  private String bearerToken;
+
+  @Override
+  public void configure() {
+
+    from("timer://test?period=2000")
+        .description("Streaming tweets from API")
+        .log("About to start our initial stream")
+        .setHeader(Exchange.HTTP_METHOD)
+        .constant(HttpMethod.GET)
+        .setHeader("Authorization")
+        .simple("Bearer " + bearerToken)
+        .to("https://api.twitter.com/2/tweets/search/recent?query=to:BBC")
+        .log("This is the status code from the response: ${header.CamelHttpResponseCode}")
+        .log("This is the return: ${body}");
+  }
+}
